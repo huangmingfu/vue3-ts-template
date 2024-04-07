15e74c8bf3922bd59802385d4e28e90bbda26482
黄明富 <212149997@qq.com>
2024-04-07 17:59:53 +0800

    fix: 细节

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,24 +1,28 @@
-import { RouteRecordRaw } from 'vue-router';
+import { RouteRecordRaw } from 'vue-router'
+
+//  meta含义
+//  title   ------------------> 标题
+//  keepAlive   ------------------> 是否缓存
 
 const routes: Array<RouteRecordRaw> = [
   {
     //重定向
     path: '/',
-    redirect: '/home',
+    redirect: '/home'
   },
   {
     path: '/home',
     name: 'Home',
     meta: {
       title: '首页',
-      keepAlive: true, //是否缓存路由
+      keepAlive: true //是否缓存路由
     },
-    component: () => import('@/pages/home/index.vue'),
+    component: () => import('@/pages/home/index.vue')
   },
   {
     name: 'NoFind',
     path: '/:pathMatch(.*)*',
-    component: () => import('@/components/Error/404.vue'),
-  },
-];
-export default routes;
+    component: () => import('@/components/Error/404.vue')
+  }
+]
+export default routes
