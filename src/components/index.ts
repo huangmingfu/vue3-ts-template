34a4558170ecd18385bac5057322ffd44ec0f6c8
黄明富 <212149997@qq.com>
2024-04-07 16:38:44 +0800

    fix: 提交规范

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ import type { App } from 'vue'
 // import { Icon } from './Icon'
 
 /* 挂载全局组件 */
-export const setupGlobCom = (app: App<Element>): void => {
+export const setupGlobCom = (_app: App<Element>): void => {
   // app.component('Icon', Icon)
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import routes from './routes';
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './routes'
 
 const router = createRouter({
   //路由模式
@@ -7,15 +7,15 @@ const router = createRouter({
   //路由的配置选项，routes为单独引进的
   routes,
   //路由导航让页面滚动到顶部
-  scrollBehavior: () => ({ left: 0, top: 0 }),
-});
+  scrollBehavior: () => ({ left: 0, top: 0 })
+})
 
 //前置守卫
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _from, next) => {
   //动态设置网页左上角的标题
-  document.title = to.meta.title || import.meta.env.VITE_APP_TITLE;
+  document.title = to.meta.title || import.meta.env.VITE_APP_TITLE
   //...判断登录token逻辑
-  next();
-});
+  next()
+})
 
-export default router;
+export default router
