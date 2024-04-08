65bf6129d7626e50aac240068e5707f559ad6886
黄明富 <212149997@qq.com>
2024-04-08 09:33:30 +0800

    fix: stylelint

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -16,8 +16,13 @@ module.exports = {
     }
   ],
   rules: {
-    'no-empty-source': null, //关闭“No empty source”规则
+    'value-keyword-case': null, // 在 css 中使用 v-bind，不报错
     'no-descending-specificity': null, // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
+    'no-empty-source': null, // 关闭禁止空源码
+    'selector-class-pattern': null, // 关闭强制选择器类名的格式
+    'property-no-unknown': null, // 禁止未知的属性(true 为不允许)
+    'value-no-vendor-prefix': null, // 关闭 属性值前缀 --webkit-box
+    'property-no-vendor-prefix': null, // 关闭 属性前缀 -webkit-mask
     'selector-pseudo-class-no-unknown': [
       true,
       {
