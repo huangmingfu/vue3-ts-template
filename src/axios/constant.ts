f6c64486d4c339209168a4cff290772faa0dedfe
黄明富 <212149997@qq.com>
2024-10-15 10:36:47 +0800

    feat: update

---
+++ src/axios/constant.ts
@@ -0,0 +1,20 @@
+type AxiosContentType =
+  | 'application/json'
+  | 'application/x-www-form-urlencoded'
+  | 'multipart/form-data'
+  | 'text/plain'
+
+/**
+ * 请求成功状态码
+ */
+export const SUCCESS_CODE = 200
+
+/**
+ * 请求contentType
+ */
+export const CONTENT_TYPE: AxiosContentType = 'application/json'
+
+/**
+ * 请求超时时间
+ */
+export const REQUEST_TIMEOUT = 60000
---
+++ src/enum/index.ts
@@ -0,0 +1,4 @@
+enum ETest {
+  a,
+  b
+}
