88742579f2387b9c7acff9387385cf529cede521
黄明富 <212149997@qq.com>
2024-12-23 09:15:48 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- transition：仅支持单个元素，需要组件内需要用根标签包裹，不如会有警告 -->
   <router-view v-slot="{ Component, route }">
     <transition :name="route.meta.transition || 'fade'">
       <keep-alive>
