290e58b31dd062084d6ba7fe1909e0cdc4fab63a
黄明富 <212149997@qq.com>
2025-01-10 10:15:27 +0800

    chore: 优化app.vue代码

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
   <!-- transition：仅支持单个元素，需要组件内需要用根标签包裹，不如会有警告 -->
   <router-view v-slot="{ Component, route }">
     <transition :name="route.meta.transition || 'fade'">
-      <keep-alive>
-        <component :is="Component" v-if="route.meta.keepAlive" :key="route.name" />
+      <keep-alive v-if="route.meta.keepAlive">
+        <component :is="Component" :key="route.name" />
       </keep-alive>
-    </transition>
-    <transition :name="route.meta.transition || 'fade'">
-      <component :is="Component" v-if="!route.meta.keepAlive" :key="route.name" />
+      <component :is="Component" v-else :key="route.name" />
     </transition>
   </router-view>
 </template>
